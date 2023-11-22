a5bd43e14337caad3964cfec8ee28d84fc423a33
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2023-11-22 16:23:26 -0500

    [Bug fix] from outgoing ctx

--- dagor/client_interceptor.go
+++ dagor/client_interceptor.go
@@ -30,7 +30,7 @@ func (d *Dagor) UnaryInterceptorClient(ctx context.Context, method string, req i
 	}
 
 	// Extracting metadata
-	md, ok := metadata.FromIncomingContext(ctx)
+	md, ok := metadata.FromOutgoingContext(ctx)
 	if !ok {
 		return errors.New("could not retrieve metadata from context")
 	}
