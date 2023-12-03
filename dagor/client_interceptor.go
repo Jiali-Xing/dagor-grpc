e8a10f01b52dbc14f6a0c0786f1fa975c759325b
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2023-12-03 01:14:58 -0500

    [New implementation] bug fix

--- dagor/client_interceptor.go
+++ dagor/client_interceptor.go
@@ -73,7 +73,7 @@ func (d *Dagor) UnaryInterceptorClient(ctx context.Context, method string, req i
 	val, ok := d.thresholdTable.Load(methodName[0])
 	if ok {
 		threshold := val.(thresholdVal)
-		if B >= threshold.Bstar && U >= threshold.Ustar {
+		if B > threshold.Bstar || (B == threshold.Bstar && U > threshold.Ustar) {
 			logger("[Ratelimiting] B %d or U %d value above the threshold B* %d or U* %d, request dropped", B, U, threshold.Bstar, threshold.Ustar)
 			return status.Errorf(codes.ResourceExhausted, "[Local Admission Control] B or U values do not meet the threshold B* or U*, request dropped")
 		}
